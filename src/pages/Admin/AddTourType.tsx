aa78d85ca69aac0e69081acc3f75f22e61484b35
Parvez Mahamud AA <parvezmahmudaa100@gmail.com>
2025-10-18 15:09:38 +0600

    38-8 Adding “Tour Type” Feature and Updating the Corresponding Backend Endpoint

--- src/pages/Admin/AddTourType.tsx
+++ src/pages/Admin/AddTourType.tsx
@@ -1,5 +1,10 @@
+import { useGetTourTypesQuery } from "@/redux/features/Tour/tour.api";
 
 export default function AddTourType() {
+
+    const { data } = useGetTourTypesQuery(undefined);
+    console.log(data);
+
   return (
     <div>AddTourType</div>
   )
