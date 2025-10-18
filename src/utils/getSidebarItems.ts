63172f1900f3c5cf2419405c7cd51db646d7974d
Parvez Mahamud AA <parvezmahmudaa100@gmail.com>
2025-10-18 11:19:36 +0600

    38-5 Recap and Discussion on Completed Work

--- src/utils/getSidebarItems.ts
+++ src/utils/getSidebarItems.ts
@@ -6,7 +6,7 @@ import { TRole } from "@/types";
 export const getSidebarItems = (userRole: TRole) => {
   switch (userRole) {
     case role.superAdmin:
-      return [...adminSidebarItems];
+      return [...adminSidebarItems, ...userSidebarItems];
     case role.admin:
       return [...adminSidebarItems];
     case role.user:
