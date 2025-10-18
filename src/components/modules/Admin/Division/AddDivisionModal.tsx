32f92eef3d8309b3a0a14d9d34fe3abc39cf80c5
Parvez Mahamud AA <parvezmahmudaa100@gmail.com>
2025-10-18 17:04:47 +0600

    39-3 Integrating Image Upload with Form Submission

--- src/components/modules/Admin/Division/AddDivisionModal.tsx
+++ src/components/modules/Admin/Division/AddDivisionModal.tsx
@@ -1,3 +1,4 @@
+import SingleImageUploader from "@/components/SingleImageUploader";
 import { Button } from "@/components/ui/button";
 import {
     Dialog,
@@ -18,9 +19,14 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
+import { useState } from "react";
 import { useForm } from "react-hook-form";
 
 export function AddDivisionModal() {
+
+  const [image, setImage] = useState<File | null>(null);
+    console.log("Inside add division modal", image);
+
   const form = useForm({
     defaultValues: {
       name: "",
@@ -78,6 +84,7 @@ export function AddDivisionModal() {
               )}
             />
           </form>
+          <SingleImageUploader onChange={setImage}/>
         </Form>
         <DialogFooter>
           <DialogClose asChild>
